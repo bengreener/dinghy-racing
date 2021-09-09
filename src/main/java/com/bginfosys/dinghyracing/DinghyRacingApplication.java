3a8bb14b4e0b33afff8ecccc3e5fa3d8372690a3
Ben Greener <53060452+bengreener@users.noreply.github.com>
2021-09-09 18:27:14 +0100

    Create initial project structure

---
+++ src/main/java/com/bginfosys/dinghyracing/DinghyRacingApplication.java
@@ -0,0 +1,13 @@
+package com.bginfosys.dinghyracing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DinghyRacingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DinghyRacingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bginfosys/dinghyracing/DinghyRacingApplicationTests.java
@@ -0,0 +1,14 @@
+package com.bginfosys.dinghyracing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DinghyRacingApplicationTests {
+
+	//Test if Application starts
+	@Test
+	void contextLoads() throws Exception {
+	}
+
+}
