bca977b6ed5be83e0325fda9be3f34c1412b266b
Ben Greener <53060452+bengreener@users.noreply.github.com>
2021-09-27 16:45:00 +0100

    Add race date

--- src/main/java/com/bginfosys/dinghyracing/Race.java
+++ src/main/java/com/bginfosys/dinghyracing/Race.java
@@ -1,5 +1,21 @@
 package com.bginfosys.dinghyracing;
 
+import java.time.LocalDate;
+
 public class Race {
+	private LocalDate date;
+	
+	public Race() {	
+	}
+	
+	public void setDate(LocalDate date) {
+		this.date = date;
+	}
+	
+	public LocalDate getDate() {
+		return date;
+	}
+	
+	
 	
 }
--- src/test/java/com/bginfosys/dinghyracing/RaceTests.java
+++ src/test/java/com/bginfosys/dinghyracing/RaceTests.java
@@ -1,22 +1,28 @@
 package com.bginfosys.dinghyracing;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-public class RaceTests {
+import java.time.LocalDate;
 
-	//@Autowired
-	private Race race;
+@SpringBootTest
+class RaceTests {
+
+	private Race race = new Race();;
 	
 	@Test
-	public void raceCreated() {
-		race = new Race();
+	void raceCreated() {
 		assertThat(race).isNotNull();
+	}
+	
+	@Test
+	void setDate() {
+		race.setDate(LocalDate.of(2021, 9, 27));
+		assertEquals(race.getDate(), LocalDate.of(2021, 9, 27));
 	}
 	
 }
