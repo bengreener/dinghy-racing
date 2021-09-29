996f4894fcdf4ac80e2f5bcb05ebc68e3bd3d21d
benhg <benhg@DESKTOP-VP7HE0G>
2021-09-29 17:49:36 +0100

    Remove import dinghyracing.race Race from RaceTests

--- src/test/java/com/bginfosys/dinghyracing/race/RaceTests.java
+++ src/test/java/com/bginfosys/dinghyracing/race/RaceTests.java
@@ -7,8 +7,6 @@ import org.junit.jupiter.api.Test;
 
 import org.springframework.boot.test.context.SpringBootTest;
 
-import com.bginfosys.dinghyracing.race.Race;
-
 import java.time.LocalDate;
 import java.time.LocalTime;
 
