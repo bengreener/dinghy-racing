453a213af30ae5839d92d6a5a5e815417d5ce5ce
benhg <benhg@DESKTOP-VP7HE0G>
2021-09-29 17:59:23 +0100

    Add RaceRepository

---
+++ src/main/java/com/bginfosys/dinghyracing/race/RaceRepository.java
@@ -0,0 +1,7 @@
+package com.bginfosys.dinghyracing.race;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface RaceRepository extends CrudRepository<Race, Long> {
+
+}
---
+++ src/test/java/com/bginfosys/dinghyracing/race/RaceRepositoryTests.java
@@ -0,0 +1,5 @@
+package com.bginfosys.dinghyracing.race;
+
+public class RaceRepositoryTests {
+
+}
