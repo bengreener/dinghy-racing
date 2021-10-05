a8ed72026c59b4f3bcaf5afeac4a56cc7f59b405
benhg <benhg@DESKTOP-VP7HE0G>
2021-10-05 12:34:42 +0100

    Add CORS access via SpringDataRestCustomization

---
+++ src/main/java/com/bginfosys/dinghyracing/race/SpringDataRestCustomization.java
@@ -0,0 +1,16 @@
+package com.bginfosys.dinghyracing.race;
+
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class SpringDataRestCustomization implements RepositoryRestConfigurer {
+
+	@Override
+	public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+		cors.addMapping("/dinghyracing/**");
+	}
+}
