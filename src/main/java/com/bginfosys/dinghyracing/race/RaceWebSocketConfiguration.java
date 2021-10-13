ad457655641cd715f618ee7aa41aa800cdf03ca0
benhg <benhg@DESKTOP-VP7HE0G>
2021-10-13 17:46:55 +0100

    Update RaceWebSocketConfiguration endpoint to avoid clash with rest api

--- src/main/java/com/bginfosys/dinghyracing/race/RaceWebSocketConfiguration.java
+++ src/main/java/com/bginfosys/dinghyracing/race/RaceWebSocketConfiguration.java
@@ -14,7 +14,7 @@ public class RaceWebSocketConfiguration implements WebSocketMessageBrokerConfigu
 		
 		@Override
 		public void registerStompEndpoints(StompEndpointRegistry registry) {
-			registry.addEndpoint("/dinghyracing").withSockJS();
+			registry.addEndpoint("/dinghyracing/messages").withSockJS();
 		}
 		
 		@Override
