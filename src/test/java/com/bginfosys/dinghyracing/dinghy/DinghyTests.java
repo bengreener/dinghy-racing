572385a1582123174164a2bfc4985dcb1dcc839d
Ben Greener <53060452+bengreener@users.noreply.github.com>
2021-12-03 17:48:52 +0000

    Add tests for Dinghy class

--- src/test/java/com/bginfosys/dinghyracing/dinghy/DinghyTests.java
+++ src/test/java/com/bginfosys/dinghyracing/dinghy/DinghyTests.java
@@ -1,5 +1,41 @@
 package com.bginfosys.dinghyracing.dinghy;
 
-public class DinghyTests {
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.Test;
+
+class DinghyTests {
+	
+	private Dinghy dinghy = new Dinghy("2689");
+	
+	@Test
+	void dinghyCreated() {
+		assertThat(dinghy).isNotNull();
+	}
+
+	@Test
+	void setId() {
+		dinghy.setId((long) 1);
+		assertEquals(dinghy.getId(), 1);
+	}
+	
+	@Test
+	void idIsLong() {
+		dinghy.setId((long) 1);
+		assertTrue(dinghy.getId() instanceof Long);
+	}
+	
+	@Test
+	void setSailNumber() {
+		dinghy.setSailNumber("1859");
+		assertEquals(dinghy.getSailNumber(), "1859");
+	}
+	
+	@Test
+	void sailNumberIsString() {
+		dinghy.setSailNumber("1859");
+		assertTrue(dinghy.getSailNumber() instanceof String);
+	}
 }
