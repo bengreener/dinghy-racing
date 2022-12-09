04f2174d7f2757aa3b65d090da697dad4fff69a9
Ben Greener <53060452+bengreener@users.noreply.github.com>
2022-12-09 18:28:04 +0000

    add profile support
    application.properties updated with dev, dev-filedb profile sections
    example local override properties added to application.properties for
    reference
    DatabaseLoader.java set to only run on dev profile
    DinghyRacingApplicationTests.java set to use dev profile for test; to
    get in memory database

--- src/test/java/com/bginfosys/dinghyracing/DinghyRacingApplicationTests.java
+++ src/test/java/com/bginfosys/dinghyracing/DinghyRacingApplicationTests.java
@@ -2,8 +2,10 @@ package com.bginfosys.dinghyracing;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("dev")
 class DinghyRacingApplicationTests {
 
 	//Test if Application starts
