b215e4da02204a4528a08606dcd5fe40a5874357
Ben Greener <53060452+bengreener@users.noreply.github.com>
2023-03-08 20:45:59 +0000

    add sail number unique within a dinghy class constraint to Dinghy

--- src/test/java/com/bginfosys/dinghyracing/persistence/DinghyRepositoryTests.java
+++ src/test/java/com/bginfosys/dinghyracing/persistence/DinghyRepositoryTests.java
@@ -6,15 +6,14 @@ import org.junit.jupiter.api.Test;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-//import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.DataIntegrityViolationException;
+
 import javax.validation.ConstraintViolationException;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 import com.bginfosys.dinghyracing.model.Dinghy;
 import com.bginfosys.dinghyracing.model.DinghyClass;
-//import com.bginfosys.dinghyracing.persistence.DinghyClassRepository;
-//import com.bginfosys.dinghyracing.persistence.DinghyRepository;
 
 @DataJpaTest
 public class DinghyRepositoryTests {
@@ -49,9 +48,28 @@ public class DinghyRepositoryTests {
 		d = dinghyRepository.save(d);
 		
 		assertThrows(ConstraintViolationException.class, () -> {
+			// force flush of memory to database
 			dinghyRepository.count();
-		});
-		
+		});	
 	}
 
+	@Test
+	void addDuplicateDinghyFails() {
+		DinghyClass dc = new DinghyClass("TestClass");
+		Dinghy d1 = new Dinghy("1234", dc);
+		Dinghy d2 = new Dinghy("1234", dc);
+		dinghyClassRepository.save(dc);
+				
+		// post original dinghy to database
+		dinghyRepository.save(d1);
+		// force flush of memory to DB
+		dinghyRepository.count();
+					
+		// confirm creating duplicate throws DataIntgrityViolationException
+		assertThrows(DataIntegrityViolationException.class, () -> {
+			dinghyRepository.save(d2);
+			// force flush of memory to database
+			dinghyRepository.count();
+		});
+	}
 }
