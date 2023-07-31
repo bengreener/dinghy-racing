3a3bc50e5882eedc17003f3b6570a4b7887f0845
Ben Greener <53060452+bengreener@users.noreply.github.com>
2023-07-31 21:41:16 +0100

    remove URI path change for DinghyClassRepository becomes dinghyClass in URLs

--- src/main/java/com/bginfosys/dinghyracing/persistence/DinghyClassRepository.java
+++ src/main/java/com/bginfosys/dinghyracing/persistence/DinghyClassRepository.java
@@ -2,11 +2,9 @@ package com.bginfosys.dinghyracing.persistence;
 
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.bginfosys.dinghyracing.model.DinghyClass;
 
-@RepositoryRestResource(path = "dinghyclasses") //, exported = false)
 public interface DinghyClassRepository extends PagingAndSortingRepository<DinghyClass, Long> {
 
 	@SuppressWarnings("unchecked")
