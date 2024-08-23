7ee81307bba3dade4f93dccd51d3673d6812b0e2
Ben Greener <53060452+bengreener@users.noreply.github.com>
2024-08-23 23:52:06 +0100

    add missing comma to sql script

--- sql_scripts/mysql_create-dinghy-racing-database.sql
+++ sql_scripts/mysql_create-dinghy-racing-database.sql
@@ -70,7 +70,7 @@ CREATE TABLE race (
 	planned_start_time DATETIME(6) NOT NULL, 
 	`type` VARCHAR(50) NOT NULL,
 	start_sequence_state VARCHAR(50),
-	start_type VARCHAR(50) NOT NULL
+	start_type VARCHAR(50) NOT NULL,
 	version BIGINT,
 	CONSTRAINT PK_race_id PRIMARY KEY (id),
 	CONSTRAINT UK_race_name_planned_start_time UNIQUE (name, planned_start_time),
