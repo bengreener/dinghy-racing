e43a96abe9835fb0dac210832ddfa2134d530e18
Ben Greener <53060452+bengreener@users.noreply.github.com>
2024-11-12 19:43:14 +0000

    update CrewController findCrewsByDinghy to return Set of CrewDTOs

--- src/main/java/com/bginfosys/dinghyracing/web/controllers/CrewController.java
+++ src/main/java/com/bginfosys/dinghyracing/web/controllers/CrewController.java
@@ -1,5 +1,8 @@
 package com.bginfosys.dinghyracing.web.controllers;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,13 +17,18 @@ import com.bginfosys.dinghyracing.web.dto.CrewDTO;
 public class CrewController {
 
 	@GetMapping(path= "/search/findCrewsByDinghy")
-	public ResponseEntity<Object> findCrewsByDinghy(@RequestParam(name = "dinghy") String dinghyUri) {
-		ResponseEntity<Object> responseEntity;
-		CrewDTO crew = new CrewDTO(new Competitor(), new Competitor());
+	public ResponseEntity<Set<CrewDTO>> findCrewsByDinghy(@RequestParam(name = "dinghy") String dinghyUri) {
+		ResponseEntity<Set<CrewDTO>> responseEntity;
+		
+		Set<CrewDTO> crews = new HashSet<CrewDTO>();
+				
+//		CrewDTO crew1 = new CrewDTO(new Competitor(), new Competitor());
+//		CrewDTO crew2 = new CrewDTO(new Competitor(), new Competitor());
+//		crews.add(new CrewDTO(new Competitor(), new Competitor()));
 		
 		responseEntity = ResponseEntity.ok()
 			.header("Content-Type", "application/hal+json")
-			.body(crew);
+			.body(crews);
 		
 		return responseEntity;
 	}
