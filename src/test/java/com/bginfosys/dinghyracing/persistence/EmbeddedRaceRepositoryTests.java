ac75d8e84d7effe86ed22516de44592fbfef6eb5
Ben Greener <53060452+bengreener@users.noreply.github.com>
2025-03-06 15:45:09 +0000

    add test EmbeddedRace with host races set is persisted

--- src/test/java/com/bginfosys/dinghyracing/persistence/EmbeddedRaceRepositoryTests.java
+++ src/test/java/com/bginfosys/dinghyracing/persistence/EmbeddedRaceRepositoryTests.java
@@ -2,12 +2,22 @@ package com.bginfosys.dinghyracing.persistence;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 
+import com.bginfosys.dinghyracing.model.DirectRace;
 import com.bginfosys.dinghyracing.model.EmbeddedRace;
+import com.bginfosys.dinghyracing.model.Fleet;
+import com.bginfosys.dinghyracing.model.Race;
+import com.bginfosys.dinghyracing.model.RaceType;
+import com.bginfosys.dinghyracing.model.StartType;
 
 @DataJpaTest
 public class EmbeddedRaceRepositoryTests {
@@ -22,6 +32,23 @@ public class EmbeddedRaceRepositoryTests {
 	void given_embeddedRaceExists_then_savesEmbeddedRace() {
 		EmbeddedRace er1 = new EmbeddedRace();
 		
+		EmbeddedRace er2 = embeddedRaceRepository.save(er1);
+				
+		assertThat(entityManager.find(EmbeddedRace.class, entityManager.getId(er2))).isEqualTo(er1);
+	}
+	
+	@Test
+	void given_embededRaceHasHostsSet_then_savesEmbeddedRace() {
+		Fleet fleet1 = new Fleet("Fleet1");
+		entityManager.persist(fleet1);
+		
+		DirectRace dr1 = new DirectRace("DR1", LocalDateTime.now(), fleet1, Duration.ofHours(1), 5, RaceType.FLEET, StartType.CSCCLUBSTART);
+		entityManager.persist(dr1);
+		
+		EmbeddedRace er1 = new EmbeddedRace();
+		Set<Race> hosts = new HashSet<Race>();
+		hosts.add(dr1);
+		er1.setHosts(hosts);
 		EmbeddedRace er2 = embeddedRaceRepository.save(er1);
 				
 		assertThat(entityManager.find(EmbeddedRace.class, entityManager.getId(er2))).isEqualTo(er1);
