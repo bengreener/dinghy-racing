e3d79068a4fa529dac85fd62ccceefe2b336536c
Ben Greener <53060452+bengreener@users.noreply.github.com>
2025-11-15 17:54:13 +0000

    add find all entries for a race to EntryRepository

--- src/main/java/com/bginfosys/dinghyracing/persistence/EntryRepository.java
+++ src/main/java/com/bginfosys/dinghyracing/persistence/EntryRepository.java
@@ -16,10 +16,16 @@
    
 package com.bginfosys.dinghyracing.persistence;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 
 import com.bginfosys.dinghyracing.model.Entry;
+import com.bginfosys.dinghyracing.model.Race;
 
 public interface EntryRepository extends JpaRepository<Entry, Long> {
+	
+	Page<Entry> findBySignedUpToRace(@Param("race") Race race, Pageable pageable);
 	
 }
